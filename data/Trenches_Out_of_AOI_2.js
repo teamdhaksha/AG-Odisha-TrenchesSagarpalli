ebab191b8e813a8fa4fa8f323c3f64f8da29701f
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-05-06 12:41:57 +0530

    Add files via upload

---
+++ data/Trenches_Out_of_AOI_2.js
@@ -0,0 +1,9 @@
+var json_Trenches_Out_of_AOI_2 = {
+"type": "FeatureCollection",
+"name": "Trenches_Out_of_AOI_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LAYER": "TRENCH", "Along_drai": "No" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 83.413690462653477, 20.563718608404255 ], [ 83.413695354502408, 20.563719459113255 ], [ 83.413699112091379, 20.563700293118167 ], [ 83.413694220243087, 20.563699442409249 ], [ 83.413690462653477, 20.563718608404255 ] ] ] ] } },
+{ "type": "Feature", "properties": { "LAYER": "TRENCH", "Along_drai": "Yes" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 83.413888775642562, 20.563859672740669 ], [ 83.413893997260203, 20.563858003111232 ], [ 83.4138884270224, 20.563842550995545 ], [ 83.413883205405185, 20.563844220624773 ], [ 83.413888775642562, 20.563859672740669 ] ] ] ] } }
+]
+}
